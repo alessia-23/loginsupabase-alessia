224545ec3b72cb2d0da29195fbd414c6db1a68e3
Alessia Perez <alessia.perez@epn.edu.ec>
2026-05-07 14:56:31 -0500

    Agregar las variables de supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://TU-PROYECTO.supabase.co',
-  supabaseKey: 'TU-ANON-KEY'
+  supabaseUrl: 'https://zdetrplxauqtthfmajrw.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkZXRycGx4YXVxdHRoZm1hanJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzQ0OTksImV4cCI6MjA5Mzc1MDQ5OX0.5uCFPX-0Aq_3zXuFF50choI298uB4x8LClUinjbWaaw'
 };
